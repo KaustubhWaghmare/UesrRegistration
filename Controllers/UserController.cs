ad925ccce4c0bb54c7b9dcd8d5ad666f62d2a07e
KaustubhWaghmare <kaustubhwaghmare1997@gmail.com>
2023-10-06 19:22:23 +0530

    UserRegistrationForm Project

---
+++ Controllers/UserController.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UserRegistration.Data;
+using UserRegistration.Models;
+
+namespace UserRegistration.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly ApplicationDbContext context;
+
+        public UserController( ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        public IActionResult Index()
+        {
+            var result = context.Users.ToList();
+            return View(result);
+        }
+        [HttpGet]
+        public IActionResult Create() 
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(User model)
+        {
+            if(ModelState.IsValid)
+            {
+                var usr = new User()
+                {
+                    Name = model.Name,
+                    Email = model.Email,
+                    DateOfBirth = model.DateOfBirth,
+                    Gender = model.Gender,
+                    Address = model.Address,
+                    ContactNumber = model.ContactNumber
+                };
+                context.Users.Add(usr);
+                context.SaveChanges();
+                TempData["error"] = "Record Saved Successfully!";
+                return RedirectToAction("index");
+            }
+            else
+            {
+                TempData["message"] = "Empty Field can't Submit";
+                return View(model);
+            }
+            
+        }
+        public IActionResult Delete(int id)
+        {   var usr=context.Users.SingleOrDefault(x => x.Id == id);
+            context.Users.Remove(usr);
+            context.SaveChanges();
+            TempData["error"] = "Record Deleted!";
+            return RedirectToAction("Index");
+        }
+        
+        public IActionResult Edit(int id)
+        {
+            var usr = context.Users.SingleOrDefault(x => x.Id == id);
+            var result = new User()
+            {
+                Name = usr.Name,
+                Email = usr.Email,
+                DateOfBirth = usr.DateOfBirth,
+                Gender = usr.Gender,
+                Address = usr.Address,
+                ContactNumber = usr.ContactNumber
+            };
+            return View(result);
+        }
+        [HttpPost]
+        public IActionResult Edit(User model)
+        {
+            var usr = new User()
+            {
+                Id = model.Id,
+                Name = model.Name,
+                Email = model.Email,
+                DateOfBirth = model.DateOfBirth,
+                Gender = model.Gender,
+                Address = model.Address,
+                ContactNumber = model.ContactNumber
+            };
+            context.Users.Update(usr);
+            context.SaveChanges();
+            TempData["error"] = "Record Updated Successfully!";
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UserRegistration.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required (ErrorMessage = "The Name Field is Required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = " The Email Field is Required")]
+        public string Email { get; set; }
+
+        [Required]
+        public DateTime DateOfBirth { get; set; }
+
+        [Required(ErrorMessage = " The Gender Field is Required")]
+        public string Gender { get; set;}
+
+        [Required(ErrorMessage = "The Address Field is Required")]
+        public string Address { get; set;}
+
+        [Required(ErrorMessage = "The Contact Number Field is Required")]
+        public string ContactNumber { get; set;}
+
+    }
+}
